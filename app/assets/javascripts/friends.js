e0b8ba30d3c14e8857ae8bb671fbd11ebed212dd
Baryskhan Bimat <baryskhan.bimat@ce.sdu.edu.kz>
2018-01-23 23:20:50 +0600

    View profile add add friend action

---
+++ app/assets/javascripts/friends.js
@@ -0,0 +1,6 @@
+// $(document).ready(function(){
+//     $('#friend-lookup-form').on('ajax:complete', function(event, data, status){
+//         console.log('ok');
+//         $('#results').html(data.responseText)
+//     })
+// });
